e4b3f67517aa8f2e278881c5c7c353398b4558ee
nicolesambile <nicolesambile026@gmail.com>
2026-05-15 11:46:46 +0800

    Add files via upload

---
+++ oop_python_courselab/week10_exception_handling/division_calculator.py
@@ -0,0 +1,9 @@
+try:
+    num1_nbs = float(input("Enter first number: "))
+    num2_nbs = float(input("Enter second number: "))
+    result = num1_nbs / num2_nbs
+    print("Result:", result)
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+except ValueError:
+    print("Invalid numeric input")
